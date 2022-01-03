0d28f7ec4bc4aad47a3257892b8ff081f2c3fc3b
Gleycykelly <gleycy_amaro@hotmail.com>
2022-01-03 10:02:33 -0300

    Inclusão da descrição do desafio 6

--- sexto-desafio/SextoDesafio/Program.cs
+++ sexto-desafio/SextoDesafio/Program.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*
+                            Sexto desafio do bootcamp Decola Tech 2a edição da DIO.
+                                            6/6 Sucessor Par
+            Dado um número X, retorne o menor número par maior do que X.
+
+            Entrada:
+            Uma linha contendo um número  0 < X < 107.
+
+            Saída:
+            Uma linha contendo a resposta do problema.
+
+ 
+            Exemplo de Entrada: 1	
+            Exemplo de Saída: 2
+            */
+using System;
 
 namespace SextoDesafio
 {

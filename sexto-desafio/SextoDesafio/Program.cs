66ef72599c3b219632eceea09dae82bc3de4c6ef
Gleycykelly <gleycy_amaro@hotmail.com>
2022-01-03 09:57:24 -0300

    Sexto desafio

---
+++ sexto-desafio/SextoDesafio/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SextoDesafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if(num>0 && num < 107){
+                if((num % 2) == 0){
+                    Console.WriteLine(num+2);
+                }else{
+                    Console.WriteLine(num+1);
+                }
+            }
+        }
+    
+    }
+}
